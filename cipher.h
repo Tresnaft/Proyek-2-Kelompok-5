9d70345b19321e229b58969512545eeeb7673ac9
Nur Akmal <akmalnur140@gmail.com>
2024-03-16 13:33:20 +0700

    udah bisa yagesya cuma belum rapih

---
+++ cipher.h
@@ -0,0 +1,57 @@
+#ifndef CIPHER_H
+#define CIPHER_H
+
+#include "lsbbmp.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <cstring>
+#include <string.h>
+
+
+#define MAX_MESSAGE_LENGTH 2048
+
+
+typedef struct{
+	char pesan[500];
+	char kunci[4];
+	char karakter[79]= " GHIJSXabcdLKMNOPQRef&.%$#ghijklmnopqTUVWr456stuvwxy!<>,zABCDEFYZ1237890_-*@?/";
+
+	int kuncitonum[4];
+	int pesantonum[500];
+	int peslen;
+	int isipesan;
+}utama;
+
+
+
+
+
+
+
+
+//char hasilLSB[2048];
+//int numLSB[2048];
+
+void cetak_pesan_encrypt(Enkripsi *En, utama *var);
+void cetak_matriks_encrypt(utama *var, Enkripsi *En);
+void cetak_pesan_decrypt(Dekripsi *De, utama *var);
+void cetak_matriks_decrypt(utama *var, Dekripsi *De);
+void pesan_encrypt(Enkripsi *En, utama *var);
+void pesan_decrypt(Dekripsi *De, utama *var);
+void matriks_pesan(utama *var);
+void matriks_kunci(utama *var);
+void cetak_matriks_pesan(utama *var);
+void cetak_matriks_kunci(utama *var);
+void matriks_LSB(utama *var, char hasilLSB[], int numLSB[], int batas);
+void Encrypt(Enkripsi *En, utama *var);
+int determinan(int kunci[]);
+void invers_matriks(int kunci[], int invers[]);
+int Zinv(int angka);
+int modulus(int angka);
+void Decrypt(utama *var, int numLSB[], Dekripsi *De, int batas);
+void tampilan(utama *var, Enkripsi *En, Dekripsi *De);
+
+
+
+#endif 
--- main.cpp
+++ main.cpp
@@ -1,14 +1,134 @@
+
 //#include <iostream>
 #include <stdio.h>
 #include "cipher.h"
+#include "lsbbmp.h"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	utama var[50];
-	Enkripsi En[50];
-	Dekripsi De[50];
 	BMPImage *image;
-	tampilan(var, En, De, image);
+	utama var[100];
+	Enkripsi En[100];
+	Dekripsi De[100];
+	char message[MAX_MESSAGE_LENGTH];
+	char *binaryMessage[MAX_MESSAGE_LENGTH * 8];
+	char hasil[MAX_MESSAGE_LENGTH];
+	
+	printf("Masukkan pesan yang ingin di Enripsi : ");
+	fgets(var->pesan, sizeof(var->pesan), stdin);
+	var->peslen = strlen(var->pesan);
+	if (var->pesan[var->peslen - 1] == '\n') {
+	    var->pesan[var->peslen - 1] = '\0';
+	    var->peslen--;
+	}
+	var->pesan[var->peslen]=var->pesan[var->peslen-1];
+	var->isipesan=var->peslen+1;
+	var->pesantonum[var->peslen];
+	
+	/*INPUT KUNCI*/
+	printf("Masukkan kunci untuk enkripsi (4 karakter) : ");
+	scanf("%s", var->kunci);
+	puts("");
+	/*MENCETAK MATRIKS*/
+	printf("=====Matriks Dari Pesan=====\n");
+	printf("Pesan : ");
+	int i;
+	for(i = 0;i < var->peslen;i++){
+		printf("%c", var->pesan[i]);	
+	}puts("");
+	matriks_pesan(var);
+	cetak_matriks_pesan(var);
+	
+	printf("=====Matriks dari kunci=====\n");
+	printf("Kunci : %s\n", var->kunci);
+	matriks_kunci(var);
+	cetak_matriks_kunci(var);
+	//system("pause");
+	
+	printf("===Matriks dari Pesan Enkripsi===\n");
+	Encrypt(En, var);
+	cetak_matriks_encrypt(var, En);
+	printf("=====Pesan Enkripsi=====");
+	pesan_encrypt(En, var);
+	printf("\nPesan yang sudah di enkripsi : ");
+	cetak_pesan_encrypt(En, var);
+	
+	// masukan ke gambar
+	
+	const char *filename_read = "sample.bmp";
+    const char *filename_write = "sampleout.bmp";
+	inputMessage(message, En);
+	
+	*binaryMessage = messageToBinary(message);
+	
+	// Membaca gambar BMP dari file
+    image = readBMP(filename_read);
+
+    if (!image) {
+        fprintf(stderr, "Error reading BMP file\n");
+        return 1;
+    }
+
+    // Menulis pesan ke dalam gambar
+    writeMsg(image, *binaryMessage);
+
+    // Menulis gambar dengan pesan ke file
+    writeBMP(filename_write, image);
+
+	printf("berhasil\n");
+	free(image->data);
+    free(image);
+    char command[256];
+    char buka[2];
+    snprintf(command, sizeof(command), "photos.exe %s", filename_write);
+    printf("Ingin membuka file foto? ");
+    scanf("%s", buka);
+    if (strcmp(buka, "y" )==0){
+    	system(command);
+	}
+    
+	// membaca gambar
+    printf("membaca pesan dari file %s\n", filename_read);
+    // Membaca gambar BMP dari file
+    image = readBMP(filename_write);
+    int panjangpesan;
+    int reallen;
+
+    if (!image) {
+        fprintf(stderr, "Error reading BMP file\n");
+        return 1;
+    }
+
+    // Mengambil bit terakhir dari setiap byte dalam komponen warna
+    panjangpesan = extractinfolen(image);
+    printf("%d\n", panjangpesan);
+    reallen = panjangpesan;
+ 
+    panjangpesan = panjangpesan * 4 + 8;
+	extractlsb(image, panjangpesan, reallen, hasil);
+	puts("");
+//	int batas = sizeof(hasil);
+//	printf("%d", batas);
+	int num[2048];
+	matriks_LSB(var, hasil, num, reallen);
+	
+	for(int h = 0; h < reallen; h++){
+		printf("%d ", num[h]);
+	}puts("");
+	
+    free(image->data);
+    free(image);
+    
+    printf("\n===Matriks dari Pesan Dekripsi===");
+	Decrypt(var, num, De, reallen);
+	printf("Matriks : \n");
+	cetak_matriks_decrypt(var, De);
+	pesan_decrypt(De, var);
+	printf("\n=========Pesan Dekripsi==========\n");
+	printf("Pesan Dekripsi : ");
+	cetak_pesan_decrypt(De, var);
+    
+	
 	return 0;
 }
